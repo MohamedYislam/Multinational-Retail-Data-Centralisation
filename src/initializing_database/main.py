b2c0fc445ddeef218069848f617e2c7db758871c
Mohamed Yislam <msyislam@gmail.com>
2024-05-17 09:00:39 +0400

    running main.py will now load the data in addition to creating the star schema

--- src/initializing_database/main.py
+++ src/initializing_database/main.py
@@ -3,14 +3,18 @@ from .products import create_products_table
 from .stores import create_stores_table
 from .users import create_user_table
 from .date_events import create_date_events_table
+from .cards import create_cards_table
+from .star_schema_setup import initializing_star_schema
 
 def main():
     print("Starting database initialization and data processing...")
     create_orders_table()
-    # create_products_table()
-    # create_stores_table()
-    # create_user_table()
-    # create_date_events_table()
+    create_products_table()
+    create_stores_table()
+    create_user_table()
+    create_date_events_table()
+    create_cards_table()
+    initializing_star_schema()
     print("All data processed and loaded successfully.")
 
 if __name__ == "__main__":
