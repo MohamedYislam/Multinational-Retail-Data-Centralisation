32322ff8fb2a13fe5f2a979746831967d18b4f0e
Mohamed Yislam <msyislam@gmail.com>
2024-05-21 07:05:55 +0400

    refactoring code

--- src/initializing_database/main.py
+++ src/initializing_database/main.py
@@ -8,13 +8,13 @@ from .star_schema_setup import initializing_star_schema
 
 def main():
     print("Starting database initialization and data processing...")
-    create_orders_table()
-    create_products_table()
-    create_stores_table()
-    create_user_table()
+    # create_orders_table()
+    # create_products_table()
+    # create_stores_table()
+    # create_user_table()
     create_date_events_table()
-    create_cards_table()
-    initializing_star_schema()
+    # create_cards_table()
+    # initializing_star_schema()
     print("All data processed and loaded successfully.")
 
 if __name__ == "__main__":
